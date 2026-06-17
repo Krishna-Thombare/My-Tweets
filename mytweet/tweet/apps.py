9fa6350184b284f7692545396c40f35e03a642ef
Krishna-Thombare <Krishnathombare43@gmail.com>
2026-06-17 15:51:57 +0530

    Add ready method

--- mytweet/tweet/apps.py
+++ mytweet/tweet/apps.py
@@ -3,3 +3,13 @@ from django.apps import AppConfig
 
 class TweetConfig(AppConfig):
     name = 'tweet'
+
+    def ready(self):
+        from django.contrib.auth.models import User
+        from django.core.validators import RegexValidator
+
+        username_field = User._meta.get_field('username')
+        username_field.validators = [RegexValidator(
+            regex=r'^[\w\s.@+-]+$',
+            message='Username can contain letters, numbers, spaces, and @/./+/-/_ only.'
+        )]
