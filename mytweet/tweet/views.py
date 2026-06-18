b389cbf5afd48fd772b459d941293ef55507fff0
Krishna-Thombare <Krishnathombare43@gmail.com>
2026-06-18 14:53:19 +0530

    create tweet view

--- mytweet/tweet/views.py
+++ mytweet/tweet/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from .models import Tweet
+from .forms import TweetForm
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.clickjacking import xframe_options_exempt
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -13,8 +18,26 @@ def tweet_list(request):
     
     return render(request, 'tweet_list.html', {'all_tweets': all_tweets, 'user_tweets': user_tweets})
 
+@login_required   # This decorator ensures that only authenticated users can access the view. Else redirected to the login page.
+@xframe_options_exempt   # It allows the view to be embedded in an iframe, useful for the modal form functionality.
+def tweet_create(request):
+    if request.method == 'POST':
+        form = TweetForm(request.POST, request.FILES)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            if request.GET.get('embed') == '1':
+                return HttpResponse('<script>window.parent.location.reload();</script>')
+            return redirect('tweet_list')
+    else:
+        form = TweetForm()
+    return render(request, 'tweet_form.html', {
+        'form': form,
+        'embed': request.GET.get('embed') == '1',
+    })
 
-    
+
 
 
     
