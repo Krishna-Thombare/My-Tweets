6243c3229135c4702d6e361497c6932660c91970
Krishna-Thombare <Krishnathombare43@gmail.com>
2026-06-18 14:45:57 +0530

    Add custom user admin

--- mytweet/tweet/admin.py
+++ mytweet/tweet/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Tweet, Profile
+from .models import Tweet, Profile, CustomUser
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 admin.site.register(Tweet)
 admin.site.register(Profile)
+admin.site.register(CustomUser, UserAdmin)
