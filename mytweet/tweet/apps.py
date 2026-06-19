0521b6f6ee050210ed46a1c1a71e9cd278441f87
Krishna-Thombare <Krishnathombare43@gmail.com>
2026-06-19 14:46:46 +0530

    update

--- mytweet/tweet/apps.py
+++ mytweet/tweet/apps.py
@@ -2,14 +2,5 @@ from django.apps import AppConfig
 
 
 class TweetConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'tweet'
-
-    def ready(self):
-        from django.contrib.auth.models import User
-        from django.core.validators import RegexValidator
-
-        username_field = User._meta.get_field('username')
-        username_field.validators = [RegexValidator(
-            regex=r'^[\w\s.@+-]+$',
-            message='Username can contain letters, numbers, spaces, and @/./+/-/_ only.'
-        )]
