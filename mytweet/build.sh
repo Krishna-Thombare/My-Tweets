ec8ee2bdb04cc29198eea9481db51ae946f1b68d
Krishna-Thombare <Krishnathombare43@gmail.com>
2026-06-22 17:02:21 +0530

    CA fix

--- mytweet/build.sh
+++ mytweet/build.sh
@@ -2,5 +2,11 @@
 set -o errexit
 
 pip install -r requirements.txt
+
+# Write CA cert from environment variable to file
+if [ -n "$DB_SSL_CA_CERT" ]; then
+    echo "$DB_SSL_CA_CERT" > ca.pem
+fi
+
 python manage.py collectstatic --no-input
 python manage.py migrate
