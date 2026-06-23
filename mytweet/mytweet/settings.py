a03bce88cda7f1c4f3c5a8f75700c5fd0f99e1b3
Krishna-Thombare <Krishnathombare43@gmail.com>
2026-06-23 09:49:25 +0530

    .

--- mytweet/mytweet/settings.py
+++ mytweet/mytweet/settings.py
@@ -19,7 +19,7 @@ import cloudinary
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(BASE_DIR / ".env")
+load_dotenv(BASE_DIR / ".env", override=False)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
