9e1518044be2d48f851c36c07973c2c0c1dab76c
Krishna-Thombare <Krishnathombare43@gmail.com>
2026-06-23 08:57:51 +0530

    .

--- mytweet/mytweet/settings.py
+++ mytweet/mytweet/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = []
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
