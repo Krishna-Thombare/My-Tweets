3ab0ef84980ebe01b2d440d5841aaf53e241d4a4
Krishna-Thombare <Krishnathombare43@gmail.com>
2026-07-04 13:25:52 +0530

    .

--- mytweet/tweet/admin.py
+++ mytweet/tweet/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import Tweet, Profile, CustomUser
 from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
 admin.site.register(Tweet)
 admin.site.register(Profile)
 admin.site.register(CustomUser, UserAdmin)
